27cace27c7f20bf0e666bf95fa6805980c65a881
Karolis Grinkus <karolis@adaptagency.com>
2022-03-03 09:37:19 +0200

    Display orgs in HP

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    `eslint:recommended`,
+    `plugin:react/recommended`,
+    `plugin:prettier/recommended`,
+  ],
+  settings: {
+    react: {
+      version: `detect`,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: [`react`],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: `module`,
+  },
+  rules: {
+    "arrow-body-style": [`error`, `always`],
+    "eol-last": [`error`, `always`],
+    "linebreak-style": [`error`, `unix`],
+    "no-alert": `error`,
+    "no-multiple-empty-lines": [`error`, { max: 1, maxEOF: 2 }],
+    "prettier/prettier": `error`,
+    quotes: [`error`, `backtick`],
+    "react/jsx-no-target-blank": [`error`, { allowReferrer: true }],
+  },
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `organisations`,
+          path: `${__dirname}/src/content/organizacijos`,
+        },
+      },
+      `gatsby-transformer-remark`,
     ]
 }
