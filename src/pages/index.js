96fc4538c29f2c5b3f1fdc6030bd6e64c3f41900
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2022-03-05 00:21:14 +0200

    T8505 Hardcode text with image pane

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from "react";
+
+// Components.
 import Constraint from "../components/Constraint";
 import HeroBanner from "../components/HeroBanner/HeroBanner";
-
 import Layout from "../components/Layout";
 import Section from "../components/Section";
 import { StaticImage } from "gatsby-plugin-image"
 import CtaCard from "../components/CtaCard";
 import CtaCardItem from "../components/CtaCard/CtaCardItem";
+import CtaLink from '../components/CtaLink';
+import ContentWithImage from '../components/ContentWithImage';
 
 const Page = () => {
   return (
@@ -27,6 +30,15 @@ const Page = () => {
             </CtaCard>
           </Constraint>
         </HeroBanner>
+      </Section>
+      <Section customClass="ContentWithImageSection">
+        <Constraint>
+          <ContentWithImage title="Būkime budrūs ir pasiruošę">
+            <CtaLink url="/bukime-budrus/patikima-informacija" title="Patikima informacija"/>
+            <CtaLink url="/bukime-budrus/piliecio-atmintine" title="Piliečio atmintinė"/>
+            <CtaLink url="/bukime-budrus/kaip-apsisaugoti-nuo-sukciu" title="Kaip apsisaugoti nuo sukčių?"/>
+          </ContentWithImage>
+        </Constraint>
       </Section>
     </Layout>
   );
