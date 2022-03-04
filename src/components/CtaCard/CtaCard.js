99531677603572e5ad3e81b7f16504a4187a82ef
Rokas Petrošius <imrokas@gmail.com>
2022-03-04 20:01:59 +0200

    Fix Link

--- src/components/CtaCard/CtaCard.js
+++ src/components/CtaCard/CtaCard.js
@@ -2,23 +2,24 @@ import * as React from "react";
 import PropTypes from "prop-types";
 
 import "./CtaCard.css";
+import { Link } from "gatsby";
 
 const CtaCard = ({ children, title, iconHandle, link }) => {
   return (
     <div className="CtaCard">
-      <a href={link} title={title}>
-        <div className="CtaCard__heading">
-          <div className={`CtaCard__icon CtaCard__icon--${iconHandle}`}>
+        <Link to={link} title={title}>
+          <div className="CtaCard__heading">
+            <div className={`CtaCard__icon CtaCard__icon--${iconHandle}`}>
 
+            </div>
+            <h2 className="CtaCard__title">
+              {title}
+              </h2>
           </div>
-          <h2 className="CtaCard__title">
-            {title}
-            </h2>
-        </div>
-        <div className="CtaCard__body">
-          {children}
-          </div>
-      </a>
+          <div className="CtaCard__body">
+            {children}
+            </div>
+        </Link>
     </div>
   );
 };
