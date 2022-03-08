7c36e1ca203f1fb74c8502a6039aae7c676b2765
Karolis Grinkus <karolis@adaptagency.com>
2022-03-08 12:01:46 +0200

    T8498 Enhance `ContactForm` progressively
    The form should work as a regular HTTP form, but should get
    progressively enhanced to an AJAX form if possible.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -10,36 +10,94 @@ import EmailInput from "../EmailInput";
 
 import "./ContactForm.css";
 
+const handleSubmit = (setFormState) => {
+  return (event) => {
+    const formData = new FormData(event.target);
+
+    setFormState({ error: false, loading: true, success: false });
+
+    const body = {
+      name: formData.get(`name`) || ``,
+      email: formData.get(`email`) || ``,
+      category: formData.get(`category`) || ``,
+      description: formData.get(`description`) || ``,
+      "privacy-policy-accepted": formData.get(`privacy-policy-accepted`) || ``,
+    };
+
+    fetch(event.target.action, {
+      method: event.target.method,
+      headers: {
+        "Content-Type": `application/json`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not OK`);
+        }
+        return response;
+      })
+      .then(() => {
+        setFormState({ error: false, loading: false, success: true });
+      })
+      .catch((error) => {
+        setFormState({ error: true, loading: false, success: false });
+        console.error(error);
+      });
+
+    event.preventDefault();
+  };
+};
+
 const ContactForm = ({ returnDestination = `/` }) => {
+  const [formState, setFormState] = React.useState({
+    error: false,
+    loading: false,
+    success: false,
+  });
+
+  const curriedHandleSubmit = React.useCallback(handleSubmit(setFormState), []);
+
   return (
     <div className="ContactForm">
       <div className="ContactForm__text">
         <h3>Turi idėjų? Pasidalink</h3>
         <p>
-          Šis puslapis skirtas pateikti svarbiausią informaciją apie pagalbą Ukrainai.
+          Šis puslapis skirtas pateikti svarbiausią informaciją apie pagalbą
+          Ukrainai.
         </p>
-        <p>
-          Jeigu turite idėjų kaip papildyti turinį, kviečiame susisiekti.
-        </p>
+        <p>Jeigu turite idėjų kaip papildyti turinį, kviečiame susisiekti.</p>
       </div>
       <div className="ContactForm__form">
-        <form
-          method="POST"
-          action={`https://ihv3yqhbe7.execute-api.eu-west-1.amazonaws.com/v1/submit?destination=${returnDestination}`}
-        >
-          <FormField labelFor="name" label="Vardas" type="text">
-            <TextInput
-              id="name"
-              placeholder="Vardenis Pavardenis"
-              name="name"
-            />
-          </FormField>
+        {formState.success ? (
+          <div className="ContactForm__success">
+            <p>Jūsų žinutė išsiųsta. Ačiū!</p>
+          </div>
+        ) : (
+          <form
+            method="POST"
+            action={`https://suukraina.lt/api/v1/submit?destination=${encodeURIComponent(
+              returnDestination
+            )}`}
+            onSubmit={curriedHandleSubmit}
+          >
+            <FormField labelFor="name" label="Vardas" type="text">
+              <TextInput
+                id="name"
+                placeholder="Vardenis Pavardenis"
+                name="name"
+              />
+            </FormField>
 
-          <FormField labelFor="email" label="El. paštas" type="email">
-            <EmailInput id="email" placeholder="vardas@post.lt" name="email" />
-          </FormField>
+            <FormField labelFor="email" label="El. paštas" type="email">
+              <EmailInput
+                id="email"
+                placeholder="vardas@post.lt"
+                name="email"
+              />
+            </FormField>
 
-          {/*
+            {/*
           <FormField labelFor="category" label="Kategorija" type="select">
             <SelectInput id="category" name="category">
               <option value="A">A</option>
@@ -48,33 +106,50 @@ const ContactForm = ({ returnDestination = `/` }) => {
           </FormField>
           */}
 
-          <FormField labelFor="description" label="Žinutė" type="textarea">
-            <TextArea
-              id="description"
-              placeholder="Žinutė"
-              name="description"
-            />
-          </FormField>
-
-          <div className="form-field form-field--type-checkbox">
-            <label htmlFor="privacy-policy-accepted" tabIndex="0">
-              <input
-                type="checkbox"
-                id="privacy-policy-accepted"
-                name="privacy-policy-accepted"
-                required
+            <FormField labelFor="description" label="Žinutė" type="textarea">
+              <TextArea
+                id="description"
+                placeholder="Žinutė"
+                name="description"
               />
-              Susipažinau ir sutinku su{` `}
-              <a href="/privatumo-politika" title="Privatumo politika">
-                privatumo politika
-              </a>
-            </label>
-          </div>
+            </FormField>
 
-          <div className="ContactForm__actions">
-            <Button color="transparent" type="submit" text="Siųsti" />
-          </div>
-        </form>
+            <div className="form-field form-field--type-checkbox">
+              <label htmlFor="privacy-policy-accepted" tabIndex="0">
+                <input
+                  type="checkbox"
+                  id="privacy-policy-accepted"
+                  name="privacy-policy-accepted"
+                  required
+                />
+                Susipažinau ir sutinku su{` `}
+                <a href="/privatumo-politika" title="Privatumo politika">
+                  privatumo politika
+                </a>
+              </label>
+            </div>
+
+            {!!formState.error && (
+              <p className="ContactForm__error">
+                Išsiųsti nepavyko. Bandykite dar kartą arba praneškite apie
+                technines problemas{` `}
+                <a href="mailto:suukraina@adaptagency.com">
+                  suukraina@adaptagency.com
+                </a>
+                .
+              </p>
+            )}
+
+            <div className="ContactForm__actions">
+              <Button
+                color="transparent"
+                disabled={formState.loading}
+                type="submit"
+                text="Siųsti"
+              />
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
