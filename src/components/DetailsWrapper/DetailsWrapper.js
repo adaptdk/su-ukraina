6b9f5431cdb30559db29b9797d16bf1732d71e46
Toxicom <tomas.98.leo@gmail.com>
2022-03-24 16:22:20 +0200

    T8734 Create a `<DetailsWrapper />`  component

---
+++ src/components/DetailsWrapper/DetailsWrapper.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./DetailsWrapper.css";
+import PropTypes from "prop-types";
+
+const DetailsWrapper = ({ tag, summary, children }) => {
+  const Tag = tag;
+
+  return (
+    <details className="DetailsWrapper">
+      <summary className="DetailsWrapper__summary-wrapper">
+        <Tag className="DetailsWrapper__summary">{summary}</Tag>
+      </summary>
+      {children}
+    </details>
+  );
+};
+
+DetailsWrapper.propTypes = {
+  tag: PropTypes.string,
+  summary: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default DetailsWrapper;
