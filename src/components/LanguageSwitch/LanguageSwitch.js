36fc0766069918c4be2a8eafc8baa1038ba21f35
Toxicom <tomas.98.leo@gmail.com>
2022-04-14 15:44:52 +0300

    T8942 Remove `PropTypes` from `LanguageSwitch`

--- src/components/LanguageSwitch/LanguageSwitch.js
+++ src/components/LanguageSwitch/LanguageSwitch.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import PropTypes from "prop-types";
 import { useLocation } from "@reach/router";
 
 import "./LanguageSwitch.css";
@@ -29,7 +28,5 @@ const LanguageSwitch = () => {
     </div>
   );
 };
-
-// LanguageSwitch.propTypes = {};
 
 export default LanguageSwitch;
