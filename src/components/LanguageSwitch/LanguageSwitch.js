9c7694b4cca9c07d7e0e775e7af7b6f3cfa6fd50
Toxicom <tomas.98.leo@gmail.com>
2022-04-14 15:00:06 +0300

    T8942 Add active state to `LanguageSwitch` based on pathname.

--- src/components/LanguageSwitch/LanguageSwitch.js
+++ src/components/LanguageSwitch/LanguageSwitch.js
@@ -1,16 +1,28 @@
 import * as React from "react";
 // import PropTypes from "prop-types";
+import { useLocation } from "@reach/router";
 
 import "./LanguageSwitch.css";
 
 const LanguageSwitch = () => {
+  const location = useLocation();
+  const currentLanguage = location.pathname.includes(`/ua/`) ? `ua` : `lt`;
+
   return (
     <div className="LanguageSwitch">
-      <div className="LanguageSwitch__language LanguageSwitch__language--active">
+      <div
+        className={`LanguageSwitch__language ${
+          currentLanguage === `ua` ? `LanguageSwitch__language--active` : ``
+        }`}
+      >
         <div className="LanguageSwitch__language-icon LanguageSwitch__language-icon--ua-flag-alt" />
         <div className="LanguageSwitch__language-title">UA</div>
       </div>
-      <div className="LanguageSwitch__language">
+      <div
+        className={`LanguageSwitch__language ${
+          currentLanguage === `lt` ? `LanguageSwitch__language--active` : ``
+        }`}
+      >
         <div className="LanguageSwitch__language-icon LanguageSwitch__language-icon--lt-flag" />
         <div className="LanguageSwitch__language-title">LT</div>
       </div>
