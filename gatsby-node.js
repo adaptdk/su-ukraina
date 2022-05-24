39e2e675343520ab0237c860eaf29da02a1c057e
Toxicom <tomas.98.leo@gmail.com>
2022-05-24 10:14:19 +0300

    #218 Create redirects

---
+++ gatsby-node.js
@@ -0,0 +1,69 @@
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/kaip-galiu-padeti/aukojimas/*/`,
+    toPath: `/kaip-galiu-padeti/aukojimas/#:splat`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/protesto-formos/budinkite-veikti`,
+    toPath: `/protesto-formos/budinkite-veikti/ambasada/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/protesto-formos/budinkite-veikti/`,
+    toPath: `/protesto-formos/budinkite-veikti/ambasada/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/protesto-formos/renginiai`,
+    toPath: `/renginiai/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/protesto-formos/renginiai/`,
+    toPath: `/renginiai/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/kaip-saugotis-nuo-sukciu-ir-dezinformacijos`,
+    toPath: `/informacija-lietuviams/sukciai-ir-dezinformacija/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/kaip-saugotis-nuo-sukciu-ir-dezinformacijos/`,
+    toPath: `/informacija-lietuviams/sukciai-ir-dezinformacija/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/patikima-informacija`,
+    toPath: `/patikimi-saltiniai/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/patikima-informacija/`,
+    toPath: `/patikimi-saltiniai/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/piliecio-atmintine`,
+    toPath: `/piliecio-atmintine/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/bukime-budrus/piliecio-atmintine/`,
+    toPath: `/piliecio-atmintine/`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/refugee-guide*`,
+    toPath: `/ua/refugee-guide:splat`,
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: `/help-search*`,
+    toPath: `/ua/help-search:splat`,
+    isPermanent: true,
+  });
+};
