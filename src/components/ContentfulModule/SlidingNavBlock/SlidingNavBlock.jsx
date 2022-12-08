fa62fc9875579f3d03c01b9170f6c2fba9fa0b82
Toxicom <tomas.98.leo@gmail.com>
2022-12-08 11:06:18 +0200

    #256 do not loop import

--- src/components/ContentfulModule/SlidingNavBlock/SlidingNavBlock.jsx
+++ src/components/ContentfulModule/SlidingNavBlock/SlidingNavBlock.jsx
@@ -2,10 +2,18 @@ import * as React from "react";
 
 import "./SlidingNavBlock.css";
 import Icon from "../../Icon";
-import ContentfulModule from "../ContentfulModule";
+import { ChipModule } from "../ChipModule";
 import { SlidingNavBlockPropTypes } from "./SlidingNavBlockPropTypes";
 
 const SlidingNavBlock = ({ id = ``, children, title, icon, data }) => {
+  const getModule = (data) => {
+    const type = data?.internal?.type;
+    if (type === `ContentfulChipModule`) {
+      return <ChipModule {...data} />;
+    }
+
+    return null;
+  };
   return (
     <div id={id} className="SlidingNavBlock">
       <div className="SlidingNavBlock__title-wrapper">
@@ -18,9 +26,7 @@ const SlidingNavBlock = ({ id = ``, children, title, icon, data }) => {
       </div>
       {children && <div className="SlidingNavBlock__content">{children}</div>}
       {!children && data && (
-        <div className="SlidingNavBlock__content">
-          <ContentfulModule module={data} />
-        </div>
+        <div className="SlidingNavBlock__content">{getModule(data)}</div>
       )}
     </div>
   );
--- src/components/ContentfulModule/SlidingNavBlock/SlidingNavBlockPropTypes.js
+++ src/components/ContentfulModule/SlidingNavBlock/SlidingNavBlockPropTypes.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
-// import ContentfulModulePropTypes from "../ContentfulModulePropTypes";
+import { ChipModulePropTypes } from "../ChipModule";
 
 const SlidingNavBlockPropTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
   children: PropTypes.node,
-  // data: PropTypes.shape(ContentfulModulePropTypes),
+  data: PropTypes.oneOfType([PropTypes.shape(ChipModulePropTypes)]),
 };
 
 export { SlidingNavBlockPropTypes };
