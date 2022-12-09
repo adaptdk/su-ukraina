5ea35d58db654e90226cf15c592ed0c1c2c9f170
Toxicom <tomas.98.leo@gmail.com>
2022-12-09 10:19:57 +0200

    #256 revert some deleted pages

---
+++ src/pages/ua/privacy-policy.js
@@ -0,0 +1,86 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+
+import Constraint from "../../components/Constraint";
+import ContentLayout from "../../components/ContentLayout";
+import Breadcrumbs from "../../components/Breadcrumbs";
+import PageTitle from "../../components/PageTitle";
+
+const Page = ({ data, pageContext }) => {
+  const {
+    breadcrumb: { crumbs },
+  } = pageContext;
+
+  const content = data.contents.edges.map((edge) => {
+    return {
+      ...edge.node.childMarkdownRemark.frontmatter,
+      html: edge.node.childMarkdownRemark.html,
+    };
+  })[0];
+
+  return (
+    <ContentLayout pagePath="/privacy-policy/">
+      {(!content || !content.title) && (
+        <PageTitle title="ПОЛІТИКА КОНФІДЕНЦІЙНОСТІ" />
+      )}
+
+      {!!content && (
+        <Constraint>
+          <Breadcrumbs crumbs={crumbs} />
+          <PageTitle title={content.title} />
+          <h1>{content.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: content.html }} />
+        </Constraint>
+      )}
+    </ContentLayout>
+  );
+};
+
+export const query = graphql`
+  query {
+    contents: allFile(
+      filter: {
+        sourceInstanceName: { eq: "page-contents" }
+        absolutePath: { regex: "//src/content/pages/privacy-policy.md$/" }
+      }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+            }
+            html
+          }
+        }
+      }
+    }
+  }
+`;
+
+Page.propTypes = {
+  pageContext: PropTypes.shape({
+    breadcrumb: PropTypes.shape({
+      crumbs: PropTypes.array,
+    }),
+  }),
+  data: PropTypes.shape({
+    contents: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            childMarkdownRemark: PropTypes.shape({
+              frontmatter: PropTypes.shape({
+                title: PropTypes.string,
+              }),
+            }),
+            html: PropTypes.string,
+          }),
+        })
+      ),
+    }),
+  }),
+};
+
+export default Page;
