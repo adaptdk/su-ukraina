e74aca6536f0fd498c188eb7620ba4cc1345708b
Toxicom <tomas.98.leo@gmail.com>
2023-01-09 14:19:34 +0200

    #256 fix build & remove filesystem plugin

--- src/pages/protesto-formos/budinkite-veikti/imone.js
+++ src/pages/protesto-formos/budinkite-veikti/imone.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Layout from "../../components/Layout";
+import Layout from "../../../components/Layout";
 
 const Page = () => {
   return <Layout pagePath="/protesto-formos/budinkite-veikti/imone/"></Layout>;
