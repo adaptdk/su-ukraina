db66a0be68aef0ec30abee6c66c3c32cd255be7f
Toxicom <tomas.98.leo@gmail.com>
2023-03-01 17:18:33 +0200

    #0 reduce logic

--- src/templates/helpPage.jsx
+++ src/templates/helpPage.jsx
@@ -63,7 +63,7 @@ const HelpPage = ({ path, pageContext }) => {
       icon: `foreign`,
       data: foreignOrgs,
     },
-  ].filter((item) => item?.data.length > 0);
+  ].filter((item) => item?.data?.length);
 
   return (
     <Layout
